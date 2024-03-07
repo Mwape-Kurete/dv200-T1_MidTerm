70c3238baa85b5ac0be77a803175846fda2dee1d
mwape-k <125279827+Mwape-Kurete@users.noreply.github.com>
2024-03-07 15:30:16 +0200

    debugging

--- mid_term/src/App.js
+++ mid_term/src/App.js
@@ -1,24 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+//import "./App.css";
+
+//Importing pages for linking
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+
+import Home from "./Pages/Home";
+import Charts from "./Pages/Charts";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Routes>
+        <Route index element={<Home />} />
+        <Route path="charts" element={<Charts />} />
+      </Routes>
+    </Router>
   );
 }
 
--- mid_term/src/Components/Navbar.js
+++ mid_term/src/Components/Navbar.js
@@ -1,6 +1,7 @@
 //Importing Libraries
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap-icons/font/bootstrap-icons.css";
+import { Link } from "react-router-dom";
 
 //Importing Pages
 import "../Styles/Navbar.css";
@@ -9,13 +10,13 @@ function Navbar() {
   return (
     <ul class="nav justify-content-center">
       <li class="nav-item">
-        <Link to="/" class="nav-link active">
+        <Link to="/" class="nav-link current">
           Home
         </Link>
       </li>
       <li class="nav-item">
         <Link to="/Charts" class="nav-link">
-          Link
+          View Stats
         </Link>
       </li>
     </ul>
